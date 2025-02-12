eb6fa0023854c3a3e21c9dc4716196462f8378f2
Gabriel Baril <gabriel-baril2010@hotmail.com>
2025-02-11 19:25:30 -0500

    Added sharpmake file

---
+++ flecs.sharpmake.cs
@@ -0,0 +1,26 @@
+using System.IO; // For Path.Combine
+using Sharpmake; // Contains the entire Sharpmake object library.
+
+[Generate]
+public class FlecsProject : BaseCppProject
+{
+    public FlecsProject()
+    {
+        Name = "flecs";
+        // SourceRootPath = @"[project.SharpmakeCsPath]";
+        AddTargets(TargetUtil.DefaultTarget);
+    }
+
+    [Configure]
+    public new void ConfigureAll(Project.Configuration conf, Target target)
+    {
+        base.ConfigureAll(conf, target);
+
+        conf.SolutionFolder = Constants.EXTERNAL_VS_CATEGORY;
+
+        conf.Output = Project.Configuration.OutputType.None;
+        conf.TargetPath = @"[project.SharpmakeCsPath]\out\bin\[target.Platform]-[target.Optimization]";
+        conf.IntermediatePath = @"[project.SharpmakeCsPath]\out\intermediate\[target.Platform]-[target.Optimization]";
+        conf.IncludePaths.Add(@"[project.SharpmakeCsPath]\include");
+    }
+}
