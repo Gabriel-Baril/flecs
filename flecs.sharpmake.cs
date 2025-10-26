e234181fdb4944f429befaae395285290cb2a30d
Gabriel Baril <gabriel-baril2010@hotmail.com>
2025-10-25 23:46:47 -0400

    Update flecs.sharpmake.cs

--- flecs.sharpmake.cs
+++ flecs.sharpmake.cs
@@ -2,11 +2,11 @@ using System.IO; // For Path.Combine
 using Sharpmake; // Contains the entire Sharpmake object library.
 
 [Generate]
-public class FlecsProject : BaseCppProject
+public class HdnCodeExternalFlecsProject : BaseCppProject
 {
-    public FlecsProject()
+    public HdnCodeExternalFlecsProject()
     {
-        Name = "flecs";
+        Name = "hdn.code.external.flecs";
         SourceRootPath = @"[project.SharpmakeCsPath]\distr";
         AddTargets(TargetUtil.DefaultTarget);
     }
